0f92339ac73abe87fc8b92db85805d8d893c0e23
Edouard HUBIN <ehubin@amadeus.com>
2019-07-02 16:36:09 +0200

    commit packaging script first version

---
+++ package.bash
@@ -0,0 +1,36 @@
+#!/usr/bin/bash -x
+
+find='C:\Program Files\Git\usr\bin\find.exe'
+
+TARGET_DIR="test" 
+mkdir $TARGET_DIR
+
+PREFIX="FreeRTOSv10.2.1"
+HEAP_FILE=heap_2.c
+DIRLIST="
+.
+FreeRTOS
+FreeRTOS/Source
+FreeRTOS/Source/include
+FreeRTOS/Source/portable
+FreeRTOS/Source/portable/Common
+FreeRTOS/Source/portable/MemMang
+FreeRTOS/Source/portable/GCC/ARM_CM3"
+for f in $DIRLIST
+  do
+  "$find" "$PREFIX/$f" -maxdepth 1 -type f -print | while IFS= read -r file; do
+    target="${file%/*}"
+    target="$TARGET_DIR/${target#"$PREFIX/"}"
+    mkdir -p "$target" && cp $file "$target"
+  done
+done
+
+# select memory policy
+cd $TARGET_DIR/FreeRTOS/Source/portable/MemMang
+for f in `ls *.c`
+do
+ if [ $f != $HEAP_FILE ]
+ then
+   mv $f "$f.off"
+  fi
+done
